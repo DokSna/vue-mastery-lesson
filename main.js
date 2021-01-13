d393b886dda7693e07778fb2f76623dbc2bea718
Artem <31787423+DokSna@users.noreply.github.com>
2021-01-13 22:28:18 +0300

    lesson 2 с поправкой на оригинал

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+//Add a link to your data object, and use v-bind to sync it up with an anchor tag in your HTML. Hint: you’ll be binding to the href attribute.
+
 var app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-        image: "assets/vmSocks-green.jpg",
-        altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
-    }
-})
+      product: 'Socks',
+      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+      link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    } 
+  })
+  
+  
+
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         product: "Socks",
+//         image: "assets/vmSocks-green.jpg",
+//         altText: "A pair of socks",
+//         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+//     }
+// })
 
 // let product = "Socks";
 // let app = new Vue({
