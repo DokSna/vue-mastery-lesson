f2967b33e3cce5b4a5480cea6e89c307e80fb21f
Artem <31787423+DokSna@users.noreply.github.com>
2021-01-13 22:23:33 +0300

    lesson 2

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-// let product = "Socks";
-let app = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: 'A pair of warm, fuzzy socks'
+        image: "assets/vmSocks-green.jpg",
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 })
+
+// let product = "Socks";
+// let app = new Vue({
+//     el: '#app',
+//     data: {
+//         product: "Socks",
+//         description: 'A pair of warm, fuzzy socks'
+//     }
+// })
 
 // let app = new Vue({
 //     el: '#app',
